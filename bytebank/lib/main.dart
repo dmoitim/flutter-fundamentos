cee9e3c157b3fe76d086259fce57ee79e194abbf
Devair Luiz Moitim <dmoitim@gmail.com>
2020-09-12 12:32:05 -0300

    Entendendo o uso de Widgets

--- bytebank/lib/main.dart
+++ bytebank/lib/main.dart
@@ -1,3 +1,28 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(Text("Bem-vindo ao ByteBank!", textDirection: TextDirection.ltr));
+void main() => runApp(
+      Column(
+        children: <Widget>[
+          Text(
+            "Deliever features faster",
+            textDirection: TextDirection.ltr,
+          ),
+          Text(
+            "Craft beaultiful UIs",
+            textDirection: TextDirection.ltr,
+          ),
+          Text(
+            "Craft beaultiful UIs",
+            textDirection: TextDirection.ltr,
+          ),
+          Column(
+            children: <Widget>[
+              Text(
+                "Craft beaultiful UIs",
+                textDirection: TextDirection.ltr,
+              ),
+            ],
+          )
+        ],
+      ),
+    );
