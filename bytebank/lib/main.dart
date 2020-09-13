911ff6a285f3f2703e02e0eec77aa83adb34eb38
Devair Luiz Moitim <dmoitim@gmail.com>
2020-09-13 08:53:52 -0300

    Refatorando o código

--- bytebank/lib/main.dart
+++ bytebank/lib/main.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
+        debugShowCheckedModeBanner: false,
         home: Scaffold(
+          body: ListaTransferencias(),
           appBar: AppBar(
             title: Text("Transferências"),
           ),
@@ -13,3 +15,40 @@ void main() => runApp(
         ),
       ),
     );
+
+class ListaTransferencias extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        ItemTransferencia(Transferencia(100.0, 1000)),
+        ItemTransferencia(Transferencia(200.0, 2000)),
+        ItemTransferencia(Transferencia(300.0, 3000)),
+      ],
+    );
+  }
+}
+
+class ItemTransferencia extends StatelessWidget {
+  final Transferencia _transferencia;
+
+  ItemTransferencia(this._transferencia);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        leading: Icon(Icons.monetization_on),
+        title: Text(this._transferencia.valor.toString()),
+        subtitle: Text(this._transferencia.numeroConta.toString()),
+      ),
+    );
+  }
+}
+
+class Transferencia {
+  final double valor;
+  final int numeroConta;
+
+  Transferencia(this.valor, this.numeroConta);
+}
